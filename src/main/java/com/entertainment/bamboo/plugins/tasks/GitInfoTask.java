50a4f9f222bcd694658ea124bdd47fdfac781c22
Wang, Dawei <dwang@entertainment.com>
2017-02-01 09:58:46 -0500

    use library

--- src/main/java/com/entertainment/bamboo/plugins/tasks/GitInfoTask.java
+++ src/main/java/com/entertainment/bamboo/plugins/tasks/GitInfoTask.java
@@ -6,12 +6,13 @@ import com.atlassian.bamboo.task.*;
 import java.io.File;
 import java.io.IOException;
 
-import com.entertainment.bamboo.plugins.tasks.model.GitCommitInfo;
-import com.entertainment.bamboo.plugins.tasks.util.GitRepoUtil;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.lib.Repository;
 
 import org.jetbrains.annotations.NotNull;
+
+import com.entertainment.cicd.util.GitInfoUtil;
+import com.entertainment.cicd.util.GitCommitInfo;
 
 /**
  * Created by dwang on 9/14/16.
@@ -28,13 +29,13 @@ public class GitInfoTask implements TaskType{
             repoDir= taskContext.getWorkingDirectory().getAbsolutePath();
         }
         String gitDir = repoDir + File.separator+ ".git";
-        Repository repo = GitRepoUtil.getRepository(gitDir);
+        Repository repo = GitInfoUtil.getRepository(gitDir, 3);
 
         if (repo ==null ){
             buildLogger.addErrorLogEntry("Directory " + repoDir + " is not a git repository");
             return failed(taskContext);
         }
-        GitCommitInfo commitInfo = GitRepoUtil.getCurrentCommit(repo);
+        GitCommitInfo commitInfo = GitInfoUtil.getCurrentCommit(repo);
 
         if (commitInfo==null) {
             throw new TaskException("Not a valid git repository, cannot find commit information");
